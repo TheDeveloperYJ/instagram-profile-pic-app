aee4ce23c2d72c6af4d0f693df2ec1113c18d1c3
Yash <yash@freightbro.com>
2018-11-24 01:08:23 +0530

    Chnage backend

--- lib/result.dart
+++ lib/result.dart
@@ -1,48 +1,55 @@
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' show parse;
 import 'package:zoomable_image/zoomable_image.dart';
 
-class ResultPage extends StatelessWidget{
-
+class ResultPage extends StatelessWidget {
   final String name;
 
   ResultPage(this.name);
 
-   Future<String> getProfilePic(String name) async{
-   final req = await http.post('https://instadpdownload.com/instagramstories/',body:{"username":name});
-   final res = parse(req.body);
-   final data = res.getElementsByClassName('downloadable card-img-top')[0];
-   return data.attributes['src'].toString();
+  Future<String> getProfilePic(String name) async {
+    final req =
+        await http.post('https://instadp.net', body: {"username": name});
+    final res = parse(req.body);
+    final data =
+        res.getElementsByClassName('img-responsive')[0].attributes['src'];
 
+    return data.toString();
   }
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Text('YOUR PIC'),
-      centerTitle: true,
-      backgroundColor: Colors.deepPurple,
+        title: Text('YOUR PIC'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
       ),
       body: FutureBuilder(
-      future:getProfilePic(name),
-      builder: (context,snapshot){
-        if(snapshot.hasData){
-          return Center(
-            child: Container(
-             child: ZoomableImage(NetworkImage(snapshot.data),backgroundColor: Colors.white,placeholder: Text('Getting image')),
-            ),
-          );
-        }
-        if(snapshot.hasError){
-          print(snapshot.error);
-          return Center(child: Text('Invalid username'),);
-        }
+        future: getProfilePic(name),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            if(snapshot.data.toString().isEmpty){
+              return Center(child: Text('Invalid username '),);
+            }
+            return Center(
+              child: Container(
+                child: ZoomableImage(NetworkImage(snapshot.data),
+                    backgroundColor: Colors.white,
+                    placeholder: Text('Getting image')),
+              ),
+            );
+          }
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return Center(
+              child: Text('Invalid username'),
+            );
+          }
 
-        return Center(child: CircularProgressIndicator());
-      },
-    ),
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
